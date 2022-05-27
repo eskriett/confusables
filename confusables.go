88467bea07421bcad7728380fb02de35e35d0b2c
Hayden Eskriett <hayden@eskriett.com>
2022-05-27 11:45:08 +0100

    Improve support for number extraction

--- confusables.go
+++ confusables.go
@@ -34,6 +34,12 @@ func (c *Confusables) ToASCII(s string) string {
 	var ascii strings.Builder
 
 	for _, r := range s {
+		if num := handleNumber(r); num != "" {
+			ascii.WriteString(num)
+
+			continue
+		}
+
 		if r > unicode.MaxASCII {
 			if v, ok := confusables[r]; ok {
 				c.removeMarks.Reset()
@@ -60,7 +66,27 @@ func (c *Confusables) ToASCII(s string) string {
 		ascii.WriteRune(r)
 	}
 
-	return ascii.String()
+	return norm.NFKC.String(ascii.String())
+}
+
+// ToNumber converts a string to a number.
+func (c *Confusables) ToNumber(s string) string {
+	s = c.ToASCII(s)
+
+	var number strings.Builder
+
+	for _, r := range s {
+		switch strings.ToLower(string(r)) {
+		case "o":
+			r = '0'
+		case "i", "l", "!":
+			r = '1'
+		}
+
+		number.WriteRune(r)
+	}
+
+	return number.String()
 }
 
 // AddMapping allows custom mappings to be defined for a rune.
@@ -76,6 +102,11 @@ func IsConfusable(s1, s2 string) bool {
 // ToASCII converts characters in a string to their ASCII equivalent if possible.
 func ToASCII(s string) string {
 	return New().ToASCII(s)
+}
+
+// ToNumber converts characters in a string to their numeric values if possible.
+func ToNumber(s string) string {
+	return New().ToNumber(s)
 }
 
 // ToSkeleton converts a string to its skeleton form as defined by the skeleton
@@ -126,6 +157,74 @@ func ToSkeletonDiff(s string) []Diff {
 	}
 
 	return diffs
+}
+
+func handleNumber(r rune) string {
+	switch {
+	// Dingbat Negative Circled
+	case r >= '❶' && r <= '❾':
+		r -= '❶' - '1'
+	// Dingbat Circled Sans-Serif
+	case r >= '➀' && r <= '➈':
+		r -= '➀' - '1'
+	case r == '🄋':
+		r = '0'
+	// Dingbat Negative Circled Sans-Serif
+	case r >= '➊' && r <= '➒':
+		r -= '➊' - '1'
+	case r == '🄌':
+		r = '0'
+	// Parenthesized
+	case r >= '⑴' && r <= '⑼':
+		r -= '⑴' - '1'
+	case r >= '⑽' && r <= '⒆':
+		r -= '⑾' - '1'
+
+		return "1" + string(r)
+	// Full Stop
+	case r >= '⒈' && r <= '⒐':
+		r -= '⒈' - '1'
+	case r >= '⒑' && r <= '⒚':
+		r -= '⒒' - '1'
+
+		return "1" + string(r)
+	case r == '🄀':
+		r = '0'
+	// Negative Circled
+	case r >= '⓫' && r <= '⓳':
+		r -= '⓫' - '1'
+
+		return "1" + string(r)
+	case r == '⓿':
+		r = '0'
+	// Double Circled
+	case r >= '⓵' && r <= '⓽':
+		r -= '⓵' - '1'
+	// Mathematical Bold
+	case r >= '𝟎' && r <= '𝟗':
+		r -= '𝟏' - '1'
+	// Mathematical Double-struck
+	case r >= '𝟘' && r <= '𝟡':
+		r -= '𝟙' - '1'
+	// Mathematical Sans-serif
+	case r >= '𝟢' && r <= '𝟫':
+		r -= '𝟣' - '1'
+	// Mathematical Sans-serif bold
+	case r >= '𝟬' && r <= '𝟵':
+		r -= '𝟭' - '1'
+	// Mathematical Monospace
+	case r >= '𝟶' && r <= '𝟿':
+		r -= '𝟷' - '1'
+	// Double digits various character classes
+	case r == '❿', r == '➉', r == '➓', r == '⓾':
+		return "10"
+	case r == '⒇', r == '⒛', r == '⓴':
+		return "20"
+	default:
+		return ""
+	}
+
+	return string(r)
 }
 
 func isASCII(s string) bool {
